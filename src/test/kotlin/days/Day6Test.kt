2559e8ac6040a3cab2f27a0db7bd0e2d81669f8b
butnotstupid <butnotstupid@gmail.com>
2021-12-05 14:31:29 +0300

    Day6 prep

---
+++ src/test/kotlin/days/Day6Test.kt
@@ -0,0 +1,22 @@
+package days
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is.`is`
+import org.junit.Test
+
+class Day6Test {
+
+    private val daySix = Day6()
+
+    @Test
+    fun testPartOne() {
+//        assertThat(daySix.partOne(), `is`())
+        println("Part one answer: ${daySix.partOne()}")
+    }
+
+    @Test
+    fun testPartTwo() {
+//        assertThat(daySix.partTwo(), `is`())
+        println("Part two answer: ${daySix.partTwo()}")
+    }
+}
